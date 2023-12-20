8a50111a88f0079df1322163f531715cc2108944
Evgeniya <speranskaya1991@mail.ru>
2023-12-20 23:03:00 +0500

    сохранение что-то пошло не так

--- vers4.py
+++ vers4.py
@@ -13,23 +13,20 @@ def read_file():
             print(line)
         data.close()
     except FileNotFoundError:
-        data = {}
-        print(data)
+        print(phone_book)
 
 # проверка чтения файла
 read_file()
 
 
 
-# блок команд связанный с добавлением нового контакта
+# блок команд связанный с Добавлением нового контакта
 # функция работы с файлом
 def add_in_file():
     with open(file_phone_book, 'a') as f: # тут все равно добавляется элемент поэтому перезапись не нужна
         json.dump(phone_book, f)
 
-
-
-# блок работы со списком
+# функция Добавление элемента в словарь
 def add():
     name = input('Введите имя: ')
     phone = input('Введите номер телефона: ')
@@ -41,6 +38,46 @@ def add():
 add()
 read_file()
 
+
+# Блок команд на удаление контакта
+# Удаления работы с файлом
+def del_in_file():
+    with open(file_phone_book, 'w+') as f: # тут все равно удаляется элемент поэтому перезапись не нужна
+        json.dump(phone_book, f)
+
+# Удаление из словаря
+def delete_contact():
+    name = input('Введите имя контакта, подлежащего удалению: ')
+    if (phone_book[name] == name):
+        del phone_book[name]
+        del_in_file()
+
+
+
+
+
+# проверка удаления
+delete_contact()
+read_file()
+
+
+
+# Удаление всего содержимого словаря
+# команда clear()
+
 # команды для тг-бота 
 
+
+# изменение
+
+# 23456789
+# the_dict = {"a": 1, "b": 2}
+ 
+# if not the_dict.get("A", None):
+#     print("Такой ключ не найден")
+# Такой ключ не найден
+ 
+# a = the_dict.get("a", None)
+# print(a)
+# 1
 
